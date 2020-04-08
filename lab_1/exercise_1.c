5ba6df8593adccd9fbf1db1809d42d6364610011
eoroose <eric.roose15@gmail.com>
2020-04-09 00:00:06 +0200

    lab 1 exercise 1 complete

--- lab_1/exercise_1.c
+++ lab_1/exercise_1.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
+    system("clear");
+    system("ls -l > files.txt");
+    system("sort files.txt | more");
+    return 0;
 }
